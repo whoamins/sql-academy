b60731334037e00bb8031e1a5965fc92c71531d5
whoamins <40am1ns@protonmail.com>
2021-11-21 20:59:14 +0300

    New queries from lessons

--- 3.sql
+++ 3.sql
@@ -68,3 +68,28 @@ SELECT room_id, avg(price) AS avg_price, COUNT(*) AS count FROM Reservations GRO
 
 SELECT room_id, avg(price) AS avg_price, COUNT(*) AS count FROM Reservations GROUP BY room_id HAVING avg_price > 150 ORDER BY count DESC, avg_price DESC;
 
+/* Объедините таблицы Class и Student_in_class с помощью внутреннего соединения по полям Class.id и Student_in_class.class. Выведите название класса (поле Class.name) и идентификатор ученика (поле Student_in_class.student). */
+
+SELECT Class.name, Student_in_class.student FROM Class JOIN Student_in_class ON Class.id = Student_in_class.class
+
+/* Дополните запрос из предыдущего задания, добавив ещё одно внутреннее соединение с таблицей Student. Объедините по полям Student_in_class.student и Student.id и вместо идентификатора ученика выведите его имя (поле first_name). */
+
+SELECT Class.name, Student.first_name FROM Class JOIN Student_in_class ON Class.id = Student_in_class.class JOIN Student ON Student_in_class.student = Student.id;
+
+/* Выведите названия продуктов, которые покупал член семьи со статусом "son". Для получения выборки вам нужно объединить таблицу Payments с таблицей FamilyMembers по полям family_member и member_id, а также с таблицей Goods по полям good и good_id. */
+
+SELECT good_name FROM Payments JOIN FamilyMembers ON family_member = member_id JOIN Goods ON good = good_id WHERE status = 'son';
+
+/* Выведите идентификатор (поле room_id) и среднюю оценку комнаты (поле rating, для вывода используйте псевдоним avg_score), составленную на основании отзывов из таблицы Reviews. Данная таблица связана с Reservations (таблица, где вы можете взять идентификатор комнаты) по полям reservation_id и Reservations.id. */
+
+SELECT room_id, AVG(rating) as avg_score FROM Reviews JOIN Reservations ON reservation_id = Reservations.id GROUP BY room_id
+
+/* Отсортируйте список компаний (таблица Company) по их названию в алфавитном порядке и выведите первые две записи. */
+
+SELECT * FROM Company ORDER BY name LIMIT 2
+
+/* Выведите начало (поле start_pair) и окончание (поле end_pair) второго и третьего занятия из таблицы Timepair. */
+
+SELECT start_pair, end_pair FROM Timepair LIMIT 1,2;
+
+/*  */
